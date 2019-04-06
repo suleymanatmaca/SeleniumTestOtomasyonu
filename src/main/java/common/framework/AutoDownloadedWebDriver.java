b6b118672d262f3a4f553a25150c158d0ed76815
Süleyman Atmaca <suleymanatmaca.027@gmail.com>
2019-04-06 18:45:47 +0300

    ilk commit

--- src/main/java/common/framework/AutoDownloadedWebDriver.java
+++ src/main/java/common/framework/AutoDownloadedWebDriver.java
@@ -3,14 +3,10 @@ package common.framework;
 import net.thucydides.core.webdriver.DriverSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class AutoDownloadedWebDriver implements DriverSource {
     public enum WebBrowsers {
-        CHROME, FIREFOX, IE
+        CHROME
     }
 
     private BaseConfiguration baseConfiguration = new BaseConfiguration();
@@ -19,11 +15,13 @@ public class AutoDownloadedWebDriver implements DriverSource {
     public WebDriver newDriver() {
         WebBrowsers browserType = WebBrowsers.valueOf(baseConfiguration.getProperty("browser"));
         String workingDir = System.getProperty("user.dir");
+
         switch (browserType) {
             case CHROME:
                 System.setProperty("webdriver.chrome.driver", workingDir + "\\Drivers\\chromedriver.exe");
                 ChromeDriver driver = new ChromeDriver();
                 driver.manage().window().maximize();
+                return driver;
            /* case FIREFOX:
                 System.setProperty("webdriver.gecko.driver", workingDir + "\\Drivers\\geckodriver.exe");
                 FirefoxOptions options = new FirefoxOptions()
